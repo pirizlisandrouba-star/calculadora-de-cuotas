a52d26107ef970287ddd3a080c2b1da0ad40462d
pirizlisandrouba-star <pirizlisandrouba@gmail.com>
2026-08-31 21:54:37 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,35 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        maxWidth: "900px",
+        margin: "0 auto",
+        padding: "40px",
+      }}
+    >
+      <h1
+        style={{
+          color: "#ea580c",
+          fontSize: "42px",
+        }}
+      >
+        Calculadora de Cuotas
+      </h1>
+
+      <p>
+        Descubrí cuánto estás pagando realmente por financiar tus compras.
+      </p>
+
+      <div
+        style={{
+          marginTop: "30px",
+          padding: "20px",
+          background: "white",
+          borderRadius: "16px",
+        }}
+      >
+        🚀 Calculadora en construcción
+      </div>
+    </main>
+  );
+}
